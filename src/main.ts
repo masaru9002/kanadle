b3b205ab87e5407640b641e459e0076358031c1a
masaru05 <azusa9002@gmail.com>
2025-06-01 01:28:22 +0700

    ✏️ Change jmdict source

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,9 @@ import { initializeWordService } from '@/services/wordService'
 
 async function initializeJmdict() {
   try {
-    await initializeWordService('./jmdict.json')
+    await initializeWordService(
+      'https://cdn.jsdelivr.net/gh/masaru9002/kanadle@main/public/jmdict.json',
+    )
   } catch (error) {
     console.error('Failed to initialize:', error)
   }
